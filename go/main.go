55968764577cc3bbaab289cc07fce5d94fc1705a
Jeson <bitnick2002@gmail.com>
2013-08-11 13:50:25 +0800

    commit

--- go/main.go
+++ go/main.go
@@ -4,6 +4,26 @@ import (
 	"./log"
 )
 
+// func vary(a ...int) {
+// 	log.Debug("sdf %t", a)
+// 	log.Debug("sdf %d", len(a))
+// }
+func changestring(p *string) {
+	*p = "new string"
+}
 func main() {
-	log.Debug("sdf %d", 10)
+	str := "old string"
+	p := &str
+	changestring(p)
+	log.Trace(str)
+	// var a string
+	log.Trace("hello")
+	log.Debug("hello")
+	log.Info("hello")
+	log.Warn("hello")
+	log.Error("hello")
+	log.Fatal("hello")
+	// vary(1, 2, 3)
+	// log.Debug("sdf %T", a)
+	// log.Debug("%d", log.l)
 }
